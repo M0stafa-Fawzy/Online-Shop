91688ea536bc25ddad40c173653c50503fcfcec4
Mostafa Fawzy <57078772+M0stafa-Fawzy@users.noreply.github.com>
2022-05-12 11:52:16 +0200

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -145,6 +145,30 @@ function uploadProfilePic() {
 
 uploadProfilePic();
 
+function uploadProfilePicWithCloud() {
+  userRouter.post("/profile/profilepicturewithcloud",auth,upload.single("profilepicture"),async (req, res) => {
+  const cloudinary = require("cloudinary").v2;
+
+  cloudinary.config({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
+  });
+
+  cloudinary.uploader
+    .upload(req.file.path)
+    .then((d) => console.log(d))
+    .catch((e) => console.log(e));
+  res.status(200)send("upload done");
+    },
+    (error, req, res, next) => {
+      res.status(400).send(error.message);
+    }
+  );
+}
+
+uploadProfilePicWithCloud();
+
 // userRouter.get('/profile/profilepicture', auth, async (req, res) => {
 //     try{
 //         if(!req.user.profile_picture){
