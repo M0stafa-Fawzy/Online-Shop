27d04e82fc725f41374042adc83dd39800ebc888
Mostafa Fawzy <mostafafawzy471@gmail.com>
2022-10-25 20:12:39 +0200

    get rid of unused  functionality

--- src/app.js
+++ src/app.js
@@ -1,32 +1,22 @@
 require("./db/mongoose");
 const express = require("express");
+const errorHandler = require("./middlewares/errorHandler")
 const app = express();
-const path = require("path");
-const hbs = require("hbs");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
-
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "./views"));
-hbs.registerPartials(path.join(__dirname, "./partials"));
+app.use(errorHandler)
 
 const user = require("../routes/user");
 const product = require("../routes/product");
 const order = require("../routes/order");
-const errorHandler = require("./middlewares/errorHandler")
 
 app.use("/users", user);
 app.use("/products", product);
 app.use("/orders", order);
-app.use(errorHandler)
 
 app.get("/", async (req, res) => {
-  res.render("index");
+  res.send("<h1>Online Shop</h1>");
 });
 
-app.get("/home", async (req, res) => {
-  res.render("home");
-});
 
 module.exports = app;
